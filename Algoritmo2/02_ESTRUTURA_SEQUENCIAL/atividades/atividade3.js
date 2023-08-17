c51b84a292f3fc865fdb98f10c2b408ad98825fa
Raquel Bulhoes <raquelbulhoes212@gmail.com>
2023-08-16 21:15:25 -0300

    Deletando copia da pasta Algoritmo2

--- Algoritmo2/02_ESTRUTURA_SEQUENCIAL/atividades/atividade3.js
+++
@@ -1,8 +0,0 @@
-let precoQuilo, consumo, valorPagar 
-
-precoQuilo = Number(prompt("Digite o valor do kilo "))
-consumo = Number(prompt("Consumo do cliente em (gr)"))
-
-valorPagar = consumo * (precoQuilo/1000)
-
-document.write(`O valor a pagar R$${valorPagar}`)
--- Algoritmo2/02_ESTRUTURA_SEQUENCIAL/atividades/atividade4.js
+++
@@ -1,9 +0,0 @@
-let medicamento, valorMed, promocao
-
-medicamento = prompt("Informe o medicamento")
-valorMed = Number(prompt("Digite o valor do medicamento"))
-
-promocao = Math.floor(valorMed * 2)
-
-
-document.write(`O valor do descontro é ${promocao}`)
