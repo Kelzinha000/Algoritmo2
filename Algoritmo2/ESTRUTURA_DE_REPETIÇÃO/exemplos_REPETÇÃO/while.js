11edf53f2fede886a82f2313d47acb577e086dd4
Kelzinha000 <138785661+Kelzinha000@users.noreply.github.com>
2023-08-31 10:49:55 -0300

    Add files via upload
    atividades de vetor e organizando o repositorios 31/08

---
+++ Algoritmo2/ESTRUTURA_DE_REPETIÇÃO/exemplos_REPETÇÃO/while.js
@@ -0,0 +1,17 @@
+/*while(condição){
+                     quando não se tem noção de quando acaba
+
+}*/
+
+let num = 10 
+
+ while(num <= 20){
+    console.log(`${num}`)
+
+    //num += 5
+    num = num + 5
+} 
+
+for(let i = 10; i<=20; i=i+5){
+
+}
